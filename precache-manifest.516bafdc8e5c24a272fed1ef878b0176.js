20a16e1da57ebc4e87d792c800c9165d92599515
lam1051999 <lam1051999@gmail.com>
2020-04-08 18:57:34 +0700

    Updates

---
+++ precache-manifest.516bafdc8e5c24a272fed1ef878b0176.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b548c4fa438908e90ffe15d1ed7a0aac",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "17eb85e32546166dfc5e",
+    "url": "/my-resume/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "b0879a9ca1146d279e0b",
+    "url": "/my-resume/static/css/main.f9a405c3.chunk.css"
+  },
+  {
+    "revision": "17eb85e32546166dfc5e",
+    "url": "/my-resume/static/js/2.fd3d3ff8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-resume/static/js/2.fd3d3ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0879a9ca1146d279e0b",
+    "url": "/my-resume/static/js/main.f1d1e2b3.chunk.js"
+  },
+  {
+    "revision": "b6292f88623cca93dae8",
+    "url": "/my-resume/static/js/runtime-main.b586985e.js"
+  },
+  {
+    "revision": "5cb12da0237866497fe94f55c1a05a9c",
+    "url": "/my-resume/static/media/app1.5cb12da0.png"
+  },
+  {
+    "revision": "a8f8959d8d61f705a280998a930a9d4f",
+    "url": "/my-resume/static/media/app2.a8f8959d.png"
+  },
+  {
+    "revision": "3bcda88b0e8251fca145af504b95dbf2",
+    "url": "/my-resume/static/media/avatar.3bcda88b.jpeg"
+  },
+  {
+    "revision": "6306fda246ff25a97204f17a558bc541",
+    "url": "/my-resume/static/media/background.6306fda2.jpeg"
+  },
+  {
+    "revision": "fd253ae25ae8c19deeab12c640f2cfc1",
+    "url": "/my-resume/static/media/expertise.fd253ae2.png"
+  },
+  {
+    "revision": "7428c0e4a2d2b5c1314d3f98f7ccbcbb",
+    "url": "/my-resume/static/media/fbnewpng.7428c0e4.png"
+  },
+  {
+    "revision": "c077f1d55fac5d71dd7c8e31ba8db1ac",
+    "url": "/my-resume/static/media/linkedin.c077f1d5.png"
+  },
+  {
+    "revision": "8c77dbee60fc56c7832f2bc68d2a377b",
+    "url": "/my-resume/static/media/passion1.8c77dbee.jpg"
+  },
+  {
+    "revision": "55e7f665c86dfafcbcfe4f22d61fb6d9",
+    "url": "/my-resume/static/media/passion2.55e7f665.jpg"
+  },
+  {
+    "revision": "f70016fcfde61bad15fade7a2965f876",
+    "url": "/my-resume/static/media/passion3.f70016fc.jpg"
+  },
+  {
+    "revision": "8e0dd39479a3462a7f55d987ef0f0d9b",
+    "url": "/my-resume/static/media/passions.8e0dd394.png"
+  },
+  {
+    "revision": "1356f85043a0ff8540fed51bf7382643",
+    "url": "/my-resume/static/media/projects.1356f850.png"
+  },
+  {
+    "revision": "08182fea116b4cc33fe0f7eddb05e01e",
+    "url": "/my-resume/static/media/services.08182fea.png"
+  },
+  {
+    "revision": "b642a66623af2a161b1e415ffee95bd9",
+    "url": "/my-resume/static/media/web1.b642a666.png"
+  },
+  {
+    "revision": "0440fd50c61146eef9d97334b87f4dcd",
+    "url": "/my-resume/static/media/web2.0440fd50.png"
+  }
+]);
